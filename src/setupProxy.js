b6428871d773b121d892709aa3bf5fcd6839b622
KyKyRuZa <ituhvatullin8@gmail.com>
2025-05-27 19:11:09 +0300

    upd url

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     ['/messages', '/media'],
     createProxyMiddleware({
-      target: 'http://localhost:8000',
+      target: 'http://anotsenimzhizn.ru/',
       changeOrigin: true,
       onProxyReq: (proxyReq, req, res) => {
         console.log('Проксирование запроса:', req.method, req.path);
