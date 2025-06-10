66e97aa8801fee0f40bf01f78c6a1ccc82896b3c
KyKyRuZa <ituhvatullin8@gmail.com>
2025-06-10 20:32:01 +0300

    upd

--- src/api/project.js
+++ src/api/project.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000/api/projects'; // Замените на ваш адрес, если другой
+const API_BASE_URL = 'https://anotsenimzhizn.ru/api/programs';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
