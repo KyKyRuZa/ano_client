a021f6160ee83ca846358dd87734a5f7b116f03f
KyKyRuZa <ituhvatullin8@gmail.com>
2025-06-12 14:36:34 +0300

    et

--- src/components/ux/Header.jsx
+++ src/components/ux/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import logo from '../style/assets/logo.svg';
+import logo from '../../style/assets/logo.svg';
 import projectsAPI from '../../api/project';
 import programsAPI from '../../api/programm';
 
