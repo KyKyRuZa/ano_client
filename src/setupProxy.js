953761f2fa8a02da3b4a2224442db7863e719ac7
KyKyRuZa <ituhvatullin8@gmail.com>
2025-06-18 12:59:02 +0300

    clear

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,14 +1,14 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
-  app.use(
-    ['/messages', '/media'],
-    createProxyMiddleware({
-      target: 'http://anotsenimzhizn.ru/',
-      changeOrigin: true,
-      onProxyReq: (proxyReq, req, res) => {
-        console.log('Проксирование запроса:', req.method, req.path);
-      },
-    })
-  );
-};
+// module.exports = function(app) {
+//   app.use(
+//     ['/messages', '/media'],
+//     createProxyMiddleware({
+//       target: 'http://anotsenimzhizn.ru/',
+//       changeOrigin: true,
+//       onProxyReq: (proxyReq, req, res) => {
+//         console.log('Проксирование запроса:', req.method, req.path);
+//       },
+//     })
+//   );
+// };
