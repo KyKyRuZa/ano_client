00092bf266fa70f9d46dc721b4debb23ee1f6385
KyKyRuZa <ituhvatullin8@gmail.com>
2025-06-20 12:19:13 +0300

    1

--- src/pages/Staff.jsx
+++ src/pages/Staff.jsx
@@ -57,7 +57,7 @@ const StaffPage = () => {
               <div className="staff-card-avatar">
                 {member.media ? (
                   <img 
-                    src={`https://anotsenimzhizn.ru${member.media}`} 
+                    src={member.media}
                     alt={member.fullname} 
                   />
                 ) : (
